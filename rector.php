4b26d5b945628ee0ed3f3f7d88e3c1e20d29330f
Viktor Szépe <viktor@szepe.net>
2020-10-08 11:15:33 +0000

    Automate refactor

---
+++ rector.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Downgrade sustainabil-it/phpstan-higher-order-collections to PHP 7.1
+ */
+
+declare(strict_types=1);
+
+use Rector\Core\Configuration\Option;
+use Rector\Set\ValueObject\SetList;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $parameters = $containerConfigurator->parameters();
+    $parameters->set(Option::SETS, [
+        SetList::DOWNGRADE_PHP74,
+    ]);
+};
